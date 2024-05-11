e3735f2f4fd001311caa72cf28514af8139c88e6
joshua <wanhoyinjoshua@yahoo.comgit config --global user.name wanhoyinjoshuagit config --global user.name wanhoyinjoshua>
2024-05-12 06:08:42 +1000

    hi

--- app/components/ButtonPanel.tsx
+++ app/components/ButtonPanel.tsx
@@ -11,32 +11,32 @@ const buttontype=props.selectedimpairment[props.impairmentcount]["class"][0]
 const dict={
     "coor":
     {
-        "yes":" coor yes",
-        "no":" coor no"
+        "yes":"Has measurable coordination deficits.",
+        "no":" No measurable coordination deficits."
     },
 
     "sensation":
     {
-        "yes":"sensation eyes",
-        "no":"sensation no"
+        "yes":"Has altered sensation.",
+        "no":"Sensation is intact."
     },
 
     "rom":
     {
-        "yes":"rom tees",
-        "no":"rom no"
+        "yes":"Range of motion is restricted.",
+        "no":"No restriction."
     },
 
     "eccentric_str":
     {
-        "yes":"ecentrci es",
-        "no":"eccentric no"
+        "yes":"Unable to control eccentric movements.",
+        "no":"Able to control eccentric movements."
     },
 
     "ex_mus_ac":
     {
-        "yes":"ex yes",
-        "no":"ex no"
+        "yes":"Has excessive muscle activity/ spasticity.",
+        "no":"Does not have excessive muscle activity."
     },
     
 
@@ -140,11 +140,11 @@ function handleno(){
              
     
          }}>
-           {message.yes}</button>
+           {message&&message["yes"]}</button>
            <button  className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto" onClick={()=>{
              handleno()
     
-         }}>  {message.no}</button>
+         }}>  {message&&message["no"]}</button>
     
                      </section>)
 
