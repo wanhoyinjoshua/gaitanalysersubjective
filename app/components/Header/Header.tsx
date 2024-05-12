6aae785b10cf1e3dbd8304fc8a5b58d7369a37b8
joshua <wanhoyinjoshua@yahoo.comgit config --global user.name wanhoyinjoshuagit config --global user.name wanhoyinjoshua>
2024-05-13 05:29:07 +1000

    hi

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -23,7 +23,7 @@ const Header = () => {
                 <span className="sr-only">Your Company</span>
                 <img
                   className="h-8 w-auto"
-                  src="/pictures/Mast_2_cropped.svg"
+                  src="/pictures/logo.svg"
                   alt=""
                 />
               </a>
@@ -58,7 +58,7 @@ const Header = () => {
                 <a href="#" className="-m-1.5 p-1.5">
                   <span className="sr-only">MAST</span>
                   <Image
-                  src={"/pictures/Mast_2_cropped.svg"}
+                  src={"/pictures/logo.svg"}
                   width={32}
                   height={32}
                   alt='logo'
